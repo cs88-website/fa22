a81aa16e26cf29d5a2f9f59e69f4f3b2c6b9c374
Michael Ball <cycomachead@gmail.com>
2022-09-06 12:34:12 -0700

    Deploy website Tue Sep  6 12:34:10 PDT 2022

--- assets/slides/fa22/03-Loops.py
+++ assets/slides/fa22/03-Loops.py
@@ -9,8 +9,17 @@ def max(x, y):
     """Returns the larger value of arguments x and y
     >>> max(6, 0)
     6
+    >>> max(0, 10)
+    10
     """
     return x if x > y else y
+
+
+def print_max(x, y):
+    if x > y:
+        print( x )
+    else:
+        print ( y )
 
 
 def countdown(num):
