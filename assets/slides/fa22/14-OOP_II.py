fb30716548faf11e5284cdd3f92757a8ca5bbc4f
Michael Ball <cycomachead@gmail.com>
2022-10-11 12:57:29 -0700

    Deploy website Tue Oct 11 12:57:28 PDT 2022

--- assets/slides/fa22/14-OOP_II.py
+++ assets/slides/fa22/14-OOP_II.py
@@ -16,25 +16,27 @@ class Point:
 
     def __str__(self):
         """Returns a nicely readable format when we call print()"""
-        # TIP: there is a new way to format strings in Python, which we don't cover:
-        # return f'Point (x: {self.x}, y: {self.y})'
-        return "Point (x: " + str(self.x) + ", y: " + str(self.y) + ")"
+        # Remember, this is just shorthand:
+        # return "Point (x: " + str(self.x) + ", y: " + str(self.y) + ")"
+        return f'Point (x: {self.x}, y: {self.y})'
 
-class Account:
+class BaseAccount:
     """Create named accounts with a balance that is
     - increased by account_deposit
     - decreased by account_withdrawl
     """
     # Class astributes outside and class defs
     _account_number_seed = 1000
+    _all_accounts = []
 
     # Constructor
     def __init__(self, name, initial_deposit):
         # Initialize the instance attributes
         self._name = name
-        self._acct_no = Account._account_number_seed
-        Account._account_number_seed += 1
+        self._acct_no = BaseAccount._account_number_seed
+        BaseAccount._account_number_seed += 1
         self._balance = initial_deposit
+        BaseAccount._all_accounts.append(self)
 
     # Selectors
     def account_name(self):
@@ -60,16 +62,20 @@ class Account:
 
     # Display representation
     def __repr__(self):
-        return '<' + str(self.account_type()) + 'Account: ' + str(self.account_name()) + '-' + str(self.account_number()) + '>'
+        return f'<{self.account_type()}Account: {self.account_name()}-{self.account_number()}>'
 
     # Print representation
     def __str__(self):
-        return str(self.account_type()) + 'Account: ' + str(self.account_name()) + '-' + str(self.account_number()) + ' Balance: ' + str(self._balance)
+        return f'{self.account_type()}Account: {self.account_name()}-{self.account_number()} Balance: {self._balance}'
 
-class CheckingAccount(Account):
+    def show_accounts():
+        for account in BaseAccount._all_accounts:
+            print(account)
+
+class CheckingAccount(BaseAccount):
     def __init__(self, name, initial_deposit):
         # Use superclass initializer
-        Account.__init__(self, name, initial_deposit)
+        BaseAccount.__init__(self, name, initial_deposit)
         # Alternatively:
         # super().__init__(name, initial_deposit)
         # Additional initialization
@@ -82,13 +88,13 @@ class CheckingAccount(Account):
     def show_superclass(self):
         return super()
 
-class SavingsAccount(Account):
+class SavingsAccount(BaseAccount):
     interest_rate = 0.02
 
     def __init__(self, name, initial_deposit):
         # Use superclass initializer
         super().__init__(name, initial_deposit)
-        # Account.__init__(self, name, initial_deposit)
+        # BaseAccount.__init__(self, name, initial_deposit)
         # Additional initialization
         self._type = "Savings"
 
@@ -105,3 +111,6 @@ class SavingsAccount(Account):
     # Print representation
     def __str__(self):
         return self.__repr__()
+
+
+cs88 = CheckingAccount('CS88', 1000)
